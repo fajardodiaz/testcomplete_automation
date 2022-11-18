12ac4e679911e024226b9a2de101938ead13d495
Pablo Fajardo <pfajardodiaz17@gmail.com>
2022-11-18 08:58:13 -0600

    First commit

---
+++ PRUEBA_ALERTA_JAVASCRIPT/Script/Unit2.js
@@ -0,0 +1,14 @@
+﻿function add_to_Cart(){
+  
+  var page = Sys.Browser("*").Page("*")
+  aqUtils.Delay(1000);
+  
+  var AddToCart = page.FindChildByXPath("//a[contains(text(),'Add to cart')]")
+  
+  AddToCart.Click();
+  
+  aqUtils.Delay(2000);
+  
+  page.Confirm().Exists;
+  
+  }
